a9f4f8d313dfb59a8f0b4ed9fd9bc7562b6cee5e
pollmix <polboy777@gmail.com>
2023-05-15 11:28:50 -0400

    added files

---
+++ src/AreaCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AreaCalculator {
+    public static void calculator() {
+        Scanner input = new Scanner(System.in);
+
+        int a = Integer.parseInt(input.next());
+        System.out.println(a);
+
+//        double fahrenheit;
+//        System.out.print("Enter the value of fahrenheit: ");
+//        fahrenheit = input.nextDouble();
+//        double celsius = (5.0 / 9) * (fahrenheit - 32);
+//        System.out.print("Temperature in Celsius is: " + celsius);
+//        int a = 0;
+//        a = a + 1;
+//        a += 1;
+//        a++; // increment
+//        ++a;
+//        // a = 4;
+//        a = 0;
+//        int b = a++; // post-increment - updated after the statement
+//        System.out.println("a is " + a + "  b is " + b);
+//
+//        a = 0;
+//        int c = ++a; //pre-increment - updated first, then run the statement
+//        System.out.println("a is " + a + "  c is " + c);
+
+//        byte i = 100;
+//        long k = i * 3 + 4;
+//        double d = i * 3.1 + k / 2;
+//        System.out.println(i + " " + k + " " + d);
+
+//        int a = 1;
+//        int b = 2;
+//        double c = (a+b)/2; // integer 1 => double 1.0
+//        System.out.println(c);
+//        double d = (a+b)/2.0; // double
+//        System.out.println(d);
+
+
+    }
+}
---
+++ src/MathQuiz.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MathQuiz {
+    public static void quiz() {
+        // generate two random numbers ---
+//        System.out.println("A random number " + (int) (Math.random() * 100));
+
+        // take two numbers from the user
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter the first number: ");
+        int num1 = (int) (Math.random() * 100);
+//        System.out.print("Enter the second number: ");
+        int num2 = (int) (Math.random() * 100);
+
+        int result = num1 + num2;
+        // take input from the user
+        System.out.println("What is " + num1 + " + " + num2 + " = ?" );
+        int userResult = input.nextInt();
+        // match the input with the computed result & give an output
+        if (result == userResult) {
+            System.out.println("Your answer is CORRECT!");
+        } else {
+            System.out.println("Your answer is WRONG!");
+        }
+    }
+}
